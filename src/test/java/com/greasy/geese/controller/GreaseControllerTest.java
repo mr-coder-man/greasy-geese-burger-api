9a847f78b63a11e3aa83797c6fe3bad3b943a6a9
A.M. Nolan <96512828+mr-coder-man@users.noreply.github.com>
2022-01-20 13:27:53 -0700

    First test failing

--- src/test/java/com/greasy/geese/controller/GreaseControllerTest.java
+++ src/test/java/com/greasy/geese/controller/GreaseControllerTest.java
@@ -1,17 +1,12 @@
 package com.greasy.geese.controller;
 
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-
-import java.util.ArrayList;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,33 +18,14 @@ public class GreaseControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private GreaseController greaseController;
-    private ArrayList<String> itemsListExpected;
-
-    @BeforeEach
-    public void before(){
-        itemsListExpected = new ArrayList<>();
-        // set menu items map up
-        itemsListExpected.add("Single Cheese Burger");
-        itemsListExpected.add("Double Cheese Burger");
-        itemsListExpected.add("Fries");
-        itemsListExpected.add("Hot Dog");
-        itemsListExpected.add("Fountain Soda");
-    }
-
     @Test
     void returnsMenuItems() throws Exception {
         MockHttpServletRequestBuilder requestBuilder
                 = get("/menu")
                 .contentType(MediaType.APPLICATION_JSON);
 
-        MvcResult result = mockMvc.perform(requestBuilder)
-                .andExpect(status().isOk())
-                .andReturn();
-        String responseAsString = result.getResponse().getContentAsString();
-        itemsListExpected.forEach(item -> {
-            assert(responseAsString.contains(item));
-        });
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk());
     }
 
 }
