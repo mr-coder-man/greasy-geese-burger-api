f145cca676c90db8e74514fda77bb56fe2df7146
A.M. Nolan <96512828+mr-coder-man@users.noreply.github.com>
2022-01-20 22:18:15 -0700

    Menu service test now passes

--- src/main/java/com/greasy/geese/controller/GreaseController.java
+++ src/main/java/com/greasy/geese/controller/GreaseController.java
@@ -2,6 +2,7 @@ package com.greasy.geese.controller;
 
 import com.greasy.geese.model.Menu;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreaseController {
 
+    @Autowired
+    private Menu menu;
+
     @GetMapping("/menu")
     public Menu menuItems(){
-        return null;
+        return menu;
     }
 }
