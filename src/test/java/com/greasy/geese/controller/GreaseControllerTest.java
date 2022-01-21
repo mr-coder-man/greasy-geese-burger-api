60105488d0a22b239cce5e4720b00ce60d7dffc6
A.M. Nolan <96512828+mr-coder-man@users.noreply.github.com>
2022-01-20 22:04:22 -0700

    Update GreaseControllerTest.java
    Got failing test for menu items coming back... time to implement

--- src/test/java/com/greasy/geese/controller/GreaseControllerTest.java
+++ src/test/java/com/greasy/geese/controller/GreaseControllerTest.java
@@ -1,12 +1,16 @@
 package com.greasy.geese.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.util.ArrayList;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,6 +21,18 @@ public class GreaseControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    private ArrayList<String> itemsListExpected;
+
+    @BeforeEach
+    public void before(){
+        itemsListExpected = new ArrayList<>();
+        // set menu items map up
+        itemsListExpected.add("Single Cheese Burger");
+        itemsListExpected.add("Double Cheese Burger");
+        itemsListExpected.add("Fries");
+        itemsListExpected.add("Hot Dog");
+        itemsListExpected.add("Fountain Soda");
+    }
 
     @Test
     void returnsMenuItems() throws Exception {
@@ -24,8 +40,15 @@ public class GreaseControllerTest {
                 = get("/menu")
                 .contentType(MediaType.APPLICATION_JSON);
 
-        mockMvc.perform(requestBuilder)
-                .andExpect(status().isOk());
+        MvcResult result = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andReturn();
+        // get entire string from response
+        String responseAsString = result.getResponse().getContentAsString();
+        // check to make sure the response contains all menu items
+        itemsListExpected.forEach(item -> {
+            assert(responseAsString.contains(item));
+        });
     }
 
 }
